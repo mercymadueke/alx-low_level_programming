736c4cf487549933117999656568fbecfb95246f
mercymadueke <mcyamara@gmail>
2022-09-10 07:43:29 -0700

    Hexadecimal

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,24 @@
+/*
+ * File: Brennan D Baraban
+ */
+
+#include <stdio.h>
+
+/**
+ * main - Prints the lowercase alphabet in reverse.
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+	char c = 'z';
+
+	while (c >= 'a')
+	{	
+		int putchar (int c);
+		c--;
+	}
+
+	putchar('\n');
+	return (0);
+}	
