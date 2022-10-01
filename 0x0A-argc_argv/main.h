45fac5adb60c393f28256b198ed53b5f8fa49056
mercymadueke <mcyamara@gmail.com>
2022-10-01 02:21:27 -0700

    whats my name

---
+++ 0x0A-argc_argv/main.h
@@ -0,0 +1,17 @@
+#ifndef MAIN_H
+#define MAIN_H
+#include <stdio.h>
+/**
+ * void prototypes()
+ * int prototypes()
+ * char prototypes()
+ */
+int main(int __attribute__((__unused__)) argc, char *argv[]);
+int main(int argc, char __attribute__((__unused__)) *argv[]);
+int main(int argc, char *argv[]);
+int main(int argc, char *argv[]);
+int main(int argc, char *argv[]);
+int main(int argc, char *argv[]);
+int _putchar(char c)
+
+endif /*MAIN_H*/
